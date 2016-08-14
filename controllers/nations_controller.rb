da4a1b0ad38a4fed3c063d7fe5c84825f98c09c1
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-08-14 10:18:16 +0100

    nations index page working and taking you to new nation page

--- controllers/nations_controller.rb
+++ controllers/nations_controller.rb
@@ -1,9 +1,17 @@
 require_relative('../models/nation')
 
+# INDEX
+get '/nations' do
+  @nations = Nation.all
+  erb(:'nations/index')
+end
+
+# NEW
 get '/nations/new' do
   erb(:'nations/new')
 end
 
+# CREATE
 post '/nations' do
   @nation = Nation.new(params)
   @nation.save
