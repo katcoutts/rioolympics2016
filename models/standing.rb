3d254858a269a68616d1a938fbccbc42a7ee3176
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-08-15 21:30:15 +0100

    can link to nations from the table names

--- models/standing.rb
+++ models/standing.rb
@@ -22,18 +22,18 @@ class Standing
   def order_by_points()
     nation_points = []
     @nations.each do |nation|
-      nation_points << ({name: nation.name, gold_medals: nation.nation_gold_medals.count, silver_medals: nation.nation_silver_medals.count, bronze_medals: nation.nation_bronze_medals.count, points: nation.nation_points})
+      nation_points << ({name: nation.name, gold_medals: nation.nation_gold_medals.count, silver_medals: nation.nation_silver_medals.count, bronze_medals: nation.nation_bronze_medals.count, points: nation.nation_points, id: nation.id})
     end
     sorted = nation_points.sort_by { |k| [k[:points],
       k[:gold_medals],
       k[:silver_medals],
       k[:bronze_medals]]}
-      
+
       return sorted.reverse
     end
 
 
-    
+
 
   end
 
